fedf3553dc1c393b1ed1d1f99a305f245d4909df
euno4h <163136318+euno4h@users.noreply.github.com>
2024-10-23 08:47:17 -0300

    .

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 //Validação do formulário
 function validateForm(){
-    
+    let name= document.getElementById("name").value;
+    let email= document.getElementById("email").value;
+    let password=document.getElementById("password").value;
+    let errorMessage= document.getElementById("errorMessage");
+   if(                ){
+        errorMessage.textContent="Campo de nome Obrigatório.";
+        return false;
+    }
+    if(           ){
+        errorMessage.textContent="Email inválido.";
+        return false;
+    }
+    if(password <6){
+        errorMessage.textContent
+
+    }
 }
